a1a5a594783b95e6919098e9dabb47972f4bafb0
lazytoday <96898345+lazytoday@users.noreply.github.com>
2022-01-04 21:23:20 +0700

    xmonad

--- xmonad.hs
+++ xmonad.hs
@@ -10,9 +10,24 @@
 import XMonad
 import Data.Monoid
 import System.Exit
-
+import XMonad.Hooks.DynamicLog
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
+
+-- The main function.
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+-- Command to launch the bar.
+myBar = "xmobar"
+
+-- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+-- Main configuration, override the defaults to your liking.
+myConfig = def { modMask = mod4Mask }
 
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
